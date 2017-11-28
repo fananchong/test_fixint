15cd019f76d7d5be8a86d44ac5bdb5bba0770bdf
fananchong <fananchong@126.com>
2017-11-28 18:38:38 +0800

    unity

---
+++ unity/test_fixint/Assets/Main.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+
+    public float test_sin(float val)
+    {
+        return Mathf.Sin(val);
+    }
+
+    int TEST_COUNT = 20;
+
+    void myprintf(int i, float valf)
+    {
+        byte[] buf = BitConverter.GetBytes(valf);
+        string hexStr = HexToString(buf);
+        string txt = i.ToString() + ": " + hexStr;
+        if (GameObject.Find("Canvas/Text").GetComponent<Text>().text !="") {
+            txt = GameObject.Find("Canvas/Text").GetComponent<Text>().text + "\n" + txt;
+        }
+        GameObject.Find("Canvas/Text").GetComponent<Text>().text = txt;
+    }
+
+    string HexToString(byte[] bytes)
+    {
+        string hexString = "";
+        for (int i = bytes.Length - 1; i >= 0; i--)
+        {
+            hexString += bytes[i].ToString("X2");
+        }
+        return hexString;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        float valf = 0.25f;
+        myprintf(-1, valf);
+        for (int i = 0; i < TEST_COUNT; i++)
+        {
+            valf = test_sin(valf);
+            myprintf(i, valf);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

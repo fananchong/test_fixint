c1ec2caac718053037d52741f34b8b7a8bed91ca
fananchong <fananchong@126.com>
2018-05-02 18:01:26 +0800

    up

--- test_fix16.cpp
+++ test_fix16.cpp
@@ -13,7 +13,7 @@ Fix16 test_sin_by_fix16(Fix16 val)
 void test_by_fix16(int count)
 {
     Fix16 valf(double(0.25));
-    myprintf(-1, (double)valf);
+    myprintf(-1, valf);
     for (int i = 0; i < count; i++)
     {
         valf = test_sin_by_fix16(valf);
